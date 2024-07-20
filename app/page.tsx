0f786b9c8fbdde77f3b27fcc2b3cb8d60ad12734
Caleb Shaw <shawcaleb1688@gmail.com>
2024-07-19 23:07:42 -0400

    payment is working

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { CreditCard, PaymentForm } from "react-square-web-payments-sdk";
+import { submitPayment } from "./actions/actions";
 
 export default function Home() {
   const appId = "sandbox-sq0idb-ZzwSnAg_JxodGMK-gnK-gw";
-  const locationId = "LN8TP2PP4XFBR";
+  const locationId = "	LN8TP2PP4XFBR";
 
   return (
     <PaymentForm
       applicationId={appId}
       locationId={locationId}
       cardTokenizeResponseReceived={async (token) => {
-        // we’ll come back to this soon
-        console.log(token);
+        const result = await submitPayment(token.token);
+        console.log(result);
       }}
     >
       <CreditCard />
